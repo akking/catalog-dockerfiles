3f8b925044a4379a839cd95064cddd3b7a90155b
Jeff Kolb <jeff.kolb@intlfcstone.com>
2016-10-04 16:30:13 -0500

    connect.sh modifications

--- MongoDB/containers/0.1.0/mongodb-config/connect.sh
+++ MongoDB/containers/0.1.0/mongodb-config/connect.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 DIG=/opt/rancher/bin/dig
 
-#function cluster_init {
-#	sleep 10
-#	MYIP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  sed -n 2p)
-#	$DIG A $MONGO_SERVICE_NAME +short > ips.tmp
-#	mongo --eval "printjson(rs.initiate())"
-#	for member in $(cat ips.tmp); do
-#		if [ $member != $MYIP ]; then
-#			mongo --eval "printjson(rs.add('$member:27017'))"
-#			sleep 5
-#		fi
-#	done
-#
-#}
-
 function cluster_init {
 	sleep 10
 	MYIP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  sed -n 2p)
-	HOSTNAME=$(hostname)
-	$DIG A $MONGO_SERVICE_NAME +short > ips.tmp
-	mongo --eval "printjson(rs.initiate())"
-	for member in $(cat ips.tmp); do
-		if [ $member != $MYIP ]; then
-			mongo --eval "printjson(rs.add('$HOSTNAME:27017'))"
-			sleep 5
+	SERVERIPS=$($DIG A $MONGO_SERVICE_NAME +short)
+	SERVERIPS=($(echo $SERVERIPS))
+	mongo --eval "printjson(rs.initiate())" --quiet    #Initiate the replica set
+	# Determine which instances are already members of the replica set
+	SETMEMBERS=($(mongo --eval 'rs.status().members.forEach( function(f) {print( f.name )})' --quiet))
+	# SETMEMBERS is a string - make it an array
+	SETHOSTS=($(echo ${SETMEMBERS[@]%:*}))
+	# List of IPs missing from the replica set
+	MISSINGSERVERS=()
+	for i in ${SERVERIPS[@]}; do
+		skip=
+		for j in ${SETHOSTS[@]%:*}; do 
+			CHECKIP=$(echo $($DIG A $j +short))
+			echo "checking $CHECKIP"
+		sleep 3
+		[[ $i == $CHECKIP ]] && { skip=1; break; }
+		done
+	[[ -n $skip ]] || MISSINGSERVERS+=("$i")
+	done
+	
+	for member in ${MISSINGSERVERS[@]}; do
+		if [ $member != $MYIP ]
+		then 
+			MONGOHOSTNAME=$(mongo $member:27017 --eval "db.hostInfo().system.hostname" --quiet):27017
+			echo "adding $MONGOHOSTNAME"
+			mongo --eval "printjson(rs.add('$MONGOHOSTNAME'))" --quiet
 		fi
 	done
+	#for member in ${MISSINGSERVERS[@]}; do
+	#	if [ $member != $MYIP ]
+	#	then
+	#		echo "$HN is not present in the replica set - adding it now"
+	#		mongo --eval "rs.add('$HN')"
+	#		sleep 5
+	#	fi
+	#done
 }
 
 function find_master {
